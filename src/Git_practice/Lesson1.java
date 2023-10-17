7bcf907a0e662cb622c68a9fd741103c5336c79c
Maya Asgarli <asgarlimaya@gmail.com>
2023-10-17 13:48:01 -0500

    Hello everyone

--- src/Git_practice/Lesson1.java
+++ src/Git_practice/Lesson1.java
@@ -4,5 +4,9 @@ public class Lesson1 {
     public static void main(String[] args) {
         String ourGroup = "the best group";
         System.out.println(ourGroup);
+
+        System.out.println("Hi");
     }
+
 }
+
